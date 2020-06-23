02c53a7ce8647460b2c28204091ae455fd83b87f
Heenen Philipp <philipp.heenen@bircher.com>
2020-06-23 23:12:58 +0200

    add argument parsing, add message compiling

--- examples/debugMachineProtocol.py
+++ examples/debugMachineProtocol.py
@@ -1,8 +1,9 @@
 import serial
 from cobs import cobsr
+import argparse
 from enum import Enum
-from struct import *
 from dataclasses import dataclass
+import numpy as np
 
 class ParserStates(Enum):
     idle    = 0
@@ -30,28 +31,53 @@ class ParseProtocol:
         self.lastPacket = BipropellantPacket(raw=bytearray(), ACK=0, CMD=chr(0), LEN=0, rawDecoded=bytearray(), CI=0, code=0, CS=0)
         self.expectedData = 0
 
-    def parse(self, newCharacter):
+    def compileMessage(self, ACK, cmd, CI, code, data):
+        newMsg = bytearray()
+        newMsg += b'\x00'
+        newMsg.append(((ACK & 1) << 7) | ord(cmd))
+        newMsg.append(CI)
+
+        decoded = bytearray()
+        decoded.append(code)
+        decoded += data
+
+#        CS = np.uint8(-code)
+        CS = 175
+        decoded.append(CS)
+
+        encoded = cobsr.encode(decoded)
+
+        newMsg.append(len(encoded))
+        newMsg += encoded
+
+        print(newMsg)
+        return newMsg
+
+
+
+
+    def parse(self, newCharacter, verbose):
         self.lastPacket.raw.append(ord(newCharacter))
 
         if newCharacter == b'\x00':
             self.lastPacket = BipropellantPacket(raw=bytearray(), ACK=0, CMD=chr(0), LEN=0, rawDecoded=bytearray(), CI=0, code=0, CS=0)
             self.lastPacket.raw.append(ord(newCharacter))
 
-            print(end = '\n> ')
+            if verbose: print(end = '\n> ')
             self.state = ParserStates.waitForCMD
 
         elif self.state == ParserStates.waitForCMD:
             self.lastPacket.ACK = (ord(newCharacter) & 0b10000000) >> 7
             self.lastPacket.CMD = chr( ord(newCharacter) & 0b01111111 )
 
-            print('ACK:'+str(self.lastPacket.ACK),end=' ')
-            print('CMD:' + str(self.lastPacket.CMD),end=' ')
+            if verbose: print('ACK:'+str(self.lastPacket.ACK),end=' ')
+            if verbose: print('CMD:' + str(self.lastPacket.CMD),end=' ')
             self.state = ParserStates.waitForCI
 
         elif self.state == ParserStates.waitForCI:
             self.lastPacket.CI = ord(newCharacter)
 
-            print('CI:' + "{:02x}".format(self.lastPacket.CI),end=' ')
+            if verbose: print('CI:' + "{:02x}".format(self.lastPacket.CI),end=' ')
             self.state = ParserStates.waitForLEN
 
         elif self.state == ParserStates.waitForLEN:
@@ -60,7 +86,7 @@ class ParseProtocol:
             self.expectedData = self.lastPacket.LEN
             self.lastPacket.encodedData = bytearray()
 
-            print('LEN:' + "{:02x}".format(self.lastPacket.LEN),end=' ')
+            if verbose: print('LEN:' + "{:02x}".format(self.lastPacket.LEN),end=' ')
             self.state = ParserStates.collectEncodedData
 
         elif self.state == ParserStates.collectEncodedData and self.expectedData != 0:
@@ -69,29 +95,50 @@ class ParseProtocol:
                 self.lastPacket.rawDecoded = self.lastPacket.raw[:4] + cobsr.decode(self.lastPacket.raw[4:])
                 self.lastPacket.code = "{:02x}".format(self.lastPacket.rawDecoded[4])
                 if self.lastPacket.code=='26':
-                    print('Text:"'+self.lastPacket.rawDecoded[5:-1].decode("utf-8")+'"',end=' ')
+                    if verbose: print('Text:"'+self.lastPacket.rawDecoded[5:-1].decode("utf-8")+'"',end=' ')
                 elif self.lastPacket.code=='fe':
                     version = int.from_bytes(self.lastPacket.rawDecoded[5:-1], byteorder='little')
-                    print('ProtocolVersion:'+str(version),end=' ')
+                    if verbose: print('ProtocolVersion:'+str(version),end=' ')
                 else:
-                    print('Code:'+self.lastPacket.code,end=' ')
-                    print(self.lastPacket.rawDecoded[5:-1],end=' ')
+                    if verbose: print('Code:'+self.lastPacket.code,end=' ')
+                    if verbose: print(self.lastPacket.rawDecoded[5:-1],end=' ')
 
                 self.lastPacket.CS = "{:02x}".format(self.lastPacket.rawDecoded[-1])
-                print('CS:'+self.lastPacket.CS,end=' ')
+                if verbose: print('CS:'+self.lastPacket.CS,end=' ')
 
                 self.state = ParserStates.idle
 
         else:
-            print(hex, end = ' ')
+            if verbose: print('Unknown Character:', newCharacter)
+
 
         return self.lastPacket
 
 
-parseProtocol = ParseProtocol()
-ser = serial.Serial('/dev/ttyUSB0', baudrate = 115200)  # open serial port
 
-while(1):
-    receivedPacket = parseProtocol.parse(ser.read())
-    if receivedPacket.rawDecoded != bytearray():
-        print(receivedPacket)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", help="Enter Serial Port. Example: COM5 or /dev/ttyUSB0", type=str)
+    parser.add_argument("-b", "--baudrate", help="Baudrate, default 38400", type=int)
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    args = parser.parse_args()
+
+    if args.baudrate:
+        baudrate = args.baudrate
+    else:
+        baudrate = 115200
+
+    verbose = args.verbose
+
+    ser = serial.Serial(args.port, baudrate = baudrate)  # open serial port
+
+    parseProtocol = ParseProtocol()
+    
+    ser.write( parseProtocol.compileMessage(0,'R',1,0xFE,bytearray()) )
+
+    while(1):
+        verbose = True
+        receivedPacket = parseProtocol.parse(ser.read(), verbose)
+        if receivedPacket.rawDecoded != bytearray():
+            print(receivedPacket)
+    #        print('ACK', receivedPacket.ACK, 'CMD', receivedPacket.CMD, 'CI', receivedPacket.CI, 'LEN', receivedPacket.LEN, 'code', receivedPacket.code, receivedPacket.rawDecoded)
